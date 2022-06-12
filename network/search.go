8c1e9fa2909128fd525e4c65ed34e6d47b219d8f
Andrew Arrow <oneone@gmail.com>
2022-06-12 16:36:55 -0700

    url

--- network/search.go
+++ network/search.go
@@ -2,17 +2,18 @@ package network
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 	"strings"
 	"time"
 )
 
-func SearchWord(word string) string {
+func SearchWord(q string) string {
 
 	t := time.Now().Add(time.Hour * -1)
 	pa := t.Format(time.RFC3339)
 	key := os.Getenv("YOUTUBE_KEY")
-	url := fmt.Sprintf("search?part=snippet&order=date&maxResults=50&q=%s&key=%s&publishedAfter=%s", word, key, pa)
+	url := fmt.Sprintf("search?part=snippet&order=date&maxResults=50&q=%s&key=%s&publishedAfter=%s", url.QueryEscape(q), key, pa)
 	json := DoGet(url)
 	return json
 }
