f55a732934ef003b7b2b5c5dbb2f9f2e196284c3
Andrew Arrow <oneone@gmail.com>
2022-06-14 07:50:56 -0700

    redis

--- server/login.go
+++ server/login.go
@@ -1,7 +1,11 @@
 package server
 
 import (
+	"fmt"
+	"many-pw/redis"
+	"math/rand"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,4 +15,24 @@ func LoginIndex(c *gin.Context) {
 	c.HTML(http.StatusOK, "login.tmpl", gin.H{
 		"flash": "",
 	})
+}
+
+func LoginSubmit(c *gin.Context) {
+	//email := c.FormPost("email")
+	password := c.Param("password")
+	if password == os.Getenv("MANY_PW_PASSWORD") {
+		uuid := pseudoUuid()
+		redis.SetAuth(uuid)
+		c.SetCookie("auth", uuid, 3600*24*365*10, "/", "", false, true)
+	}
+	c.Redirect(http.StatusFound, "/")
+	c.Abort()
+}
+
+func pseudoUuid() string {
+
+	b := make([]byte, 16)
+	rand.Read(b)
+
+	return fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 }
--- server/server.go
+++ server/server.go
@@ -21,6 +21,7 @@ func Serve(port string) {
 	router.GET("/v/:slug/:id", VideoShow)
 	router.POST("/v/:slug/:id/gem", VideoGem)
 	router.POST("/v/:slug/:id/ungem", VideoUnGem)
+	router.POST("/login", LoginSubmit)
 	router.NoRoute(NotFoundIndex)
 
 	AddTemplates(router, prefix)
