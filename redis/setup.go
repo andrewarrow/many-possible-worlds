3a8a829e6e52d01769424f64cea16c7da5349831
Andrew Arrow <oneone@gmail.com>
2022-06-15 11:26:19 -0700

    client

--- redis/setup.go
+++ redis/setup.go
@@ -2,18 +2,29 @@ package redis
 
 import (
 	"context"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 )
 
 var ctx = context.Background()
+var globalClient = getClient()
+var lastConnect = time.Now().Unix()
 
-func nc() *redis.Client {
+func getClient() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "", // no password set
 		DB:       1,  // use default DB
 	})
 
+	lastConnect = time.Now().Unix()
+
 	return rdb
+}
+func nc() *redis.Client {
+	if time.Now().Unix()-lastConnect > 3600 {
+		globalClient = getClient()
+	}
+	return globalClient
 }
