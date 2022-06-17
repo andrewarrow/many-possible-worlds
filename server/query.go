e55c23398395c4ccc733bf51846c7b50657dc43e
Andrew Arrow <oneone@gmail.com>
2022-06-17 08:45:41 -0700

    more

--- server/query.go
+++ server/query.go
@@ -30,36 +30,28 @@ func makeQueryHTML(slug string, offset int) string {
 	buffer := []string{}
 	items := redis.QueryChannelsInSlug(slug, offset)
 	pinned := redis.QueryPinned(slug)
-	buffer = append(buffer, "<div class=\"good-links\">")
 
 	buffer = append(buffer, "<h2>Pinned</h2>")
 	for _, gem := range pinned {
-		buffer = append(buffer, "<div class=\"item\">")
-		buffer = append(buffer, "<div>")
+		buffer = append(buffer, "<p>")
 		buffer = append(buffer, fmt.Sprintf("<a href=\"/c/%s/%s\">%s</a>", slug, gem.Id, gem.Title))
 
-		buffer = append(buffer, "</div>")
-		buffer = append(buffer, "</div>")
+		buffer = append(buffer, "</p>")
 	}
 
 	buffer = append(buffer, "<h2>Latest</h2>")
+	t := `
+<a href=\"/c/%s/%s\">%s</a> with %s sub(s)<br/>%s ago
+`
 	for _, item := range items {
-		buffer = append(buffer, "<div class=\"item\">")
-		buffer = append(buffer, "<div>")
-		buffer = append(buffer, fmt.Sprintf("<a href=\"/c/%s/%s\">%s</a>", slug, item.Id, item.Title))
+		buffer = append(buffer, "<p>")
 
-		buffer = append(buffer, "</div>")
-		buffer = append(buffer, "<div class=\"small\">")
-		buffer = append(buffer, fmt.Sprintf("%s sub(s)", item.SubscriberCount))
-		buffer = append(buffer, "</div>")
-		buffer = append(buffer, "<div class=\"small\">")
-		buffer = append(buffer, DeltaAgo(item.PublishedAt)+" ago")
-		buffer = append(buffer, "</div>")
-		buffer = append(buffer, "</div>")
+		buffer = append(buffer, fmt.Sprintf(t, slug, item.Id, item.Title, item.SubscriberCount, DeltaAgo(item.PublishedAt)))
+
+		buffer = append(buffer, "</p>")
 	}
-	buffer = append(buffer, "</div>")
-	buffer = append(buffer, "<div>")
+	buffer = append(buffer, "<p>")
 	buffer = append(buffer, fmt.Sprintf("<a href=\"?offset=%d\">Load Next 50</a>", offset+50))
-	buffer = append(buffer, "</div>")
+	buffer = append(buffer, "</p>")
 	return strings.Join(buffer, "\n")
 }
