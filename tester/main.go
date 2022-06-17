960a10f714599aee1bc39bf3c8723437c1973592
Andrew Arrow <oneone@gmail.com>
2022-06-17 07:03:25 -0700

    start of min

--- tester/main.go
+++ tester/main.go
@@ -6,11 +6,17 @@ import (
 	"many-pw/network"
 )
 
-func main() {
+func main2() {
 	cid := "UCHEoWlkGf-nSVdNk9b6SR0g"
 	cmap := map[string]bool{}
 	cmap[cid] = true
 	json := network.GetChannels(cmap)
 	ioutil.WriteFile("fname.txt", []byte(json), 0644)
 	fmt.Println(cid)
+}
+
+func main() {
+	vids := []string{"faTNcxR0KqM"}
+	json := network.SearchVideos(vids)
+	ioutil.WriteFile("fname.txt", []byte(json), 0644)
 }
