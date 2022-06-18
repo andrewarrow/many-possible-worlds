731f781d686f84c31f78497963ceee94eac7100c
Andrew Arrow <oneone@gmail.com>
2022-06-18 15:57:58 -0700

    adding pub date to videos

--- redis/video.go
+++ redis/video.go
@@ -2,6 +2,7 @@ package redis
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -13,6 +14,7 @@ func LoadVideo(id string) *Video {
 	v.ImageUrl = m["img"]
 	v.Title = m["title"]
 	v.ChannelId = m["cid"]
+	v.PublishedAt, _ = strconv.ParseInt(m["pub"], 10, 64)
 	v.Id = id
 	return &v
 }
@@ -21,6 +23,7 @@ func StoreSingleVideo(v *Video) {
 	nc().HSet(ctx, v.Id, "title", v.Title).Err()
 	nc().HSet(ctx, v.Id, "cid", v.ChannelId).Err()
 	nc().HSet(ctx, v.Id, "img", v.ImageUrl).Err()
+	nc().HSet(ctx, v.Id, "pub", v.PublishedAt).Err()
 	expireTime := time.Now().Add(time.Hour * 24 * 30 * 12 * 2)
 	nc().ExpireAt(ctx, v.Id, expireTime)
 
@@ -36,9 +39,10 @@ func AddToVideoSet(cid, vid string, pub int64) {
 func FindOlderVideo(cid string, pub int64) *Video {
 	vidzset := fmt.Sprintf("%s-v", cid)
 	zrb := redis.ZRangeBy{
-		Max:   fmt.Sprintf("%d", pub),
-		Min:   "-inf",
-		Count: 1,
+		Max:    fmt.Sprintf("%d", pub),
+		Min:    "-inf",
+		Count:  1,
+		Offset: 0,
 	}
 	vals, _ := nc().ZRangeByScore(ctx, vidzset, &zrb).Result()
 	if len(vals) == 0 {
