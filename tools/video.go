34d1bb1b4681bf581e7291dc5585c85f26f0b074
Andrew Arrow <oneone@gmail.com>
2022-06-18 07:54:09 -0700

    import by vid

--- tools/video.go
+++ tools/video.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"many-pw/network"
 	"many-pw/parse"
 )
@@ -12,10 +11,13 @@ func ImportVideo(id string) {
 	if json == "" {
 		return
 	}
-	ioutil.WriteFile("fname.txt", []byte(json), 0644)
+	//ioutil.WriteFile("fname.txt", []byte(json), 0644)
 	vs := parse.ParseVideoJson(json)
 	v := vs.Items[0]
 
 	fmt.Println(v.Snippet.Title)
+	fmt.Println(v.Snippet.ChannelTitle)
+	fmt.Println(v.Snippet.ChannelId)
+	ImportChannel(v.Snippet.ChannelId)
 
 }
